854130a983096b0d89cd25dda4910ce9e3b28282
andrey <6097033725>
2020-10-28 15:01:39 +0300

    migrations

---
+++ src/migrations/m181029_103225_add_site_settings_tables.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181029_103225_add_site_data_tables
+ */
+class m181029_103225_add_site_data_tables extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+	    $tableOptions = null;
+	    if ($this->db->driverName === 'mysql') {
+		    $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+	    }
+
+	    // data categories
+	    $this->createTable('site_data_category', [
+		    'id' => $this->primaryKey(),
+		    'name' => $this->string()->notNull(),
+		    'order' => $this->smallInteger()->notNull()->defaultValue(0),
+	    ], $tableOptions);
+
+	    // data values
+	    $this->createTable('site_data', [
+		    'id' => $this->primaryKey(),
+		    'category_id' => $this->integer()->notNull(),
+		    'key' => $this->string()->notNull(),
+		    'value' => $this->text(),
+		    'type' => $this->string(10)->notNull(),
+		    'name' => $this->string()->notNull(),
+	    ], $tableOptions);
+
+	    $this->createIndex(
+		    'idx-site_data-category_id',
+		    'site_data',
+		    'category_id'
+	    );
+
+	    $this->createIndex(
+		    'ux-site_data-key',
+		    'site_data',
+		    'key',
+		    true
+	    );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex('idx-site_data-category_id', 'site_data_category');
+        $this->dropIndex('ux-site_data-key', 'site_data_category');
+        $this->dropTable('site_data');
+        $this->dropTable('site_data_category');
+    }
+}
