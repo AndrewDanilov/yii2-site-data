1952e59ec1d495ec6119d52b5cf86e975cb2bbbb
andrey <6097033725>
2020-11-02 00:18:34 +0300

    misc fixes

--- src/views/manager/edit.php
+++ src/views/manager/edit.php
@@ -22,36 +22,7 @@ $this->params['breadcrumbs'][] = $category->name;
 
 	<?php foreach ($category->data as $data) { ?>
 
-		<?php
-		switch ($data->type) {
-			case SiteData::VALUE_TYPE_REACHTEXT:
-				echo $form->field($data, '[' . $data->key . ']value')->widget(CKEditor::class, [
-					'editorOptions' => ElFinder::ckeditorOptions('elfinder', CKEditorHelper::defaultOptions()),
-				])->label($data->name);
-				break;
-			case SiteData::VALUE_TYPE_TEXT:
-				echo $form->field($data, '[' . $data->key . ']value')->textarea(['rows' => 6])->label($data->name);
-				break;
-			case SiteData::VALUE_TYPE_BOOLEAN:
-				echo $form->field($data, '[' . $data->key . ']value')->dropDownList(['0' => 'Нет', '1' => 'Да'])->label($data->name);
-				break;
-			case SiteData::VALUE_TYPE_FILE:
-				echo $form->field($data, '[' . $data->key . ']value')->widget(InputFile::class, [
-					'language'      => 'ru',
-					'controller'    => 'elfinder', // вставляем название контроллера, по умолчанию равен elfinder
-					'template'      => '<div class="input-group">{input}<span class="input-group-btn">{button}</span></div>',
-					'options'       => ['class' => 'form-control'],
-					'buttonOptions' => ['class' => 'btn btn-default'],
-					'multiple'      => false,      // возможность выбора нескольких файлов
-				])->label($data->name);
-				break;
-			case SiteData::VALUE_TYPE_IMAGE:
-				echo $form->field($data, '[' . $data->key . ']value')->widget(InputImages::class)->label($data->name);
-				break;
-			default:
-				echo $form->field($data, '[' . $data->key . ']value')->textInput(['maxlength' => true])->label($data->name);
-		}
-		?>
+		<?= $data->formField($form, '[' . $data->key . ']value', $data->name) ?>
 
 	<?php } ?>
 
